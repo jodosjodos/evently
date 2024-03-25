40357e1d9b6928de498447805ed8b5950ebbab87
jodosjodos <jeandedieu2030@gmail.com>
2024-03-25 17:46:49 +0200

    [init] initialize project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main><Button className="bg-red-500">aAme</Button></main>
+  );
+}
