6fdd77b7304741085b004f5c4139057ac98217e8
jodosjodos <jeandedieu2030@gmail.com>
2024-03-28 19:02:23 +0200

    [fix] done with header

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 import {
   Sheet,
   SheetContent,
@@ -7,22 +7,31 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import NavItems from "./NavItems";
 
 export default function MobileNav() {
   return (
     <nav className="md:hidden">
       <Sheet>
         <SheetTrigger className="align-middle">
-            <Image src="/assets/icons/menu.svg" alt="menu" width={24} height={24} className ="cursor-pointer"/>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+          <Separator className ="border border-gray-500"/>
+          <NavItems/>
         </SheetContent>
       </Sheet>
     </nav>
