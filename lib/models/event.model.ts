0608dff1bae0e6ee01792175a561e4eaef1f12c5
jodos <jeandedieu2030@gmail.com>
2024-04-08 22:40:50 +0200

    [add] add  mongoose models

---
+++ lib/models/event.model.ts
@@ -0,0 +1,41 @@
+import { Document, Schema, model, models, Types } from "mongoose";
+
+interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category?: {
+    _id: string;
+    name: string;
+  };
+  organizer?: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+const EventSchema = new Schema<IEvent>({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, required: true },
+  endDateTime: { type: Date, required: true },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+export const Event = models.Event || model<IEvent>("Event", EventSchema);
