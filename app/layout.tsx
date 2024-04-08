43b54b03b65f860ee7861319146883b59c14ef71
jodos <jeandedieu2030@gmail.com>
2024-04-08 21:32:35 +0200

    [add] add hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "./global.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
