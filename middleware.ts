82527852e5ef53f033552adc2a04c1aa160ed734
jodosjodos <jeandedieu2030@gmail.com>
2024-04-18 19:18:28 +0200

    [fix] remove typo

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,8 @@ export default authMiddleware({
     "/api/uploadthing",
   ],
   ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
     "/api/uploadthing",
   ],
 });
